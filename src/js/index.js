25ff5d089d8215d8f45d41c1ab6d728040f25f44
i3SK <okli23@live.com>
2024-02-13 21:38:55 +0100

    Fetch funciona

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Fetch from "./component/Todo-fetch.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Fetch />, document.querySelector("#app"));
